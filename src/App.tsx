d7433643a7648d70bd0f2f539de21aa5107e60be
Oludiran-Ayoade Olutimileyin <111990787+Oludiran-Ayoade@users.noreply.github.com>
2024-06-22 06:25:10 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import ResetPassword from './pages/Auth/ResetPassword';
 import ProductDetailsPage from './pages/ProductDetailsPage';
 import CartPage from './pages/CartPage';
 import AllUsers from './pages/Admin/AllUsers'
+import OrderList from './pages/Admin/OrderList';
 import { useAuth } from './context/auth';
 
 function App() {
@@ -20,14 +21,15 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/signup" element={isAuthenticated ? <Navigate to="/" replace /> : <SignUpPage />} />
-      <Route path="/signin" element={isAuthenticated ? <Navigate to="/" replace /> : <SignInPage />} />
+      <Route path="/signup" element={ <SignUpPage />} />
+      <Route path="/signin" element={  <SignInPage />} />
       <Route path="/admin" element={isAuthenticated ? <AdminDashboard /> : <Navigate to="/signin" replace />} />
       <Route path="/allusers" element={isAuthenticated ? <AllUsers /> : <Navigate to="/signin" replace />} />
+      <Route path="/orders" element={isAuthenticated ? <OrderList /> : <Navigate to="/signin" replace />} />
       <Route path="/addproducts" element={isAuthenticated ? <AddProducts /> : <Navigate to="/signin" replace />} />
       <Route path="/updateproducts" element={isAuthenticated ? <UpdateProduct /> : <Navigate to="/signin" replace />} />
       <Route path="/forgotpassword" element={<ForgotPassword /> } />
-      <Route path="/verify-otp" element={isAuthenticated ? <Navigate to="/" replace /> : <VerifyOTP /> } />
+      <Route path="/verify-otp" element={<VerifyOTP /> } />
       <Route path="/productdetails/:id" element={<ProductDetailsPage userId={''}/>} />
       <Route path="/reset-password" element={<ResetPassword /> } />
       <Route path="/cartpage" element={isAuthenticated ? <CartPage /> : <Navigate to="/signin" replace />} />
