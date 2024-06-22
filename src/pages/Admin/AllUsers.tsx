047b46d3f697eb0d5132e4e5784d51151fb2b77a
Oludiran-Ayoade Olutimileyin <111990787+Oludiran-Ayoade@users.noreply.github.com>
2024-06-22 12:07:21 +0100

    Update AllUsers.tsx

--- src/pages/Admin/AllUsers.tsx
+++ src/pages/Admin/AllUsers.tsx
@@ -16,7 +16,7 @@ const UserTable: React.FC = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get<User[]>('http://localhost:3001/auth/getallusers');
+        const response = await axios.get<User[]>('https://quickbite-backend-1-w5az.onrender.com/auth/getallusers');
         setUsers(response.data);
       } catch (error) {
         console.error('Error fetching user details:', error);
